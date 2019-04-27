d8e00f483963a81879ab007dce4337e3b85038d0
Jim Hough <teachlife@outlook.com>
2019-04-27 22:02:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,13 @@
 # code here!
 class School
   attr_accessor
-  attr_reader :school_name
+  attr_reader :school_name, :roster
   def initialize(name)
     @school_name = name
   end
   def roster=
     @roster = {}
   end
+  def add_student (student, key)
+    
 end
